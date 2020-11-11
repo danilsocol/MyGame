e7c50249e1398d8a0fda3db2f057c003aaccfdb5
danilsocol <danilsocol-2002@mail.ru>
2020-11-11 20:49:59 +0500

    Select

--- MyGame/MyGame/Program.cs
+++ MyGame/MyGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace MyGame
 {
@@ -6,30 +7,66 @@ namespace MyGame
     {
         static void Main(string[] args)//'╔', '═', '╗' '╚', '═', '╝',║,,120
         {
-            ReadMenu();
+            int select = 1;
 
+            ReadMenu(select);
+            Selection(select);
 
             Console.Read();
         }
-        static void ReadMenu()
+        static void Selection(int select)
         {
-            ReadFream("   FEELWORDS   ");
+            while (true)
+            {
+                ConsoleKey Key = Console.ReadKey().Key;
+
+                Console.WriteLine(Key);
+
+                if(Key == ConsoleKey.Enter)
+
+
+                if (select > 1)
+                    if (Key == ConsoleKey.W || Key == ConsoleKey.UpArrow)
+                        select -= 1;
+
+                if (select < 4)
+                    if (Key == ConsoleKey.S || Key == ConsoleKey.DownArrow)
+                        select += 1;
+
+                Console.Clear();
+                ReadMenu(select);
+            }
+        }
+
+        static void ReadMenu(int select)
+        {
+            int counter = 0;
+
+            ReadFream("   FEELWORDS   ", select, counter);
             Console.WriteLine("\n");
+            counter++;
 
-            ReadFream(" Новая игра ");
+            ReadFream(" Новая игра ", select, counter);
             Console.WriteLine("");
+            counter++;
 
-            ReadFream(" Продолжить ");
+            ReadFream(" Продолжить ", select, counter);
             Console.WriteLine("");
+            counter++;
 
-            ReadFream(" Рейтинг    ");
+            ReadFream(" Рейтинг    ", select, counter);
             Console.WriteLine("");
+            counter++;
 
-            ReadFream(" Выход      ");
+            ReadFream(" Выход      ", select, counter);
             Console.WriteLine("");
+            counter++;
 
         }
-        static void ReadFream(string text)
+        enum Optoins
+        {
+        }
+        static void ReadFream(string text, int select, int counter)
         {
             int numSpace = 50;
             if(text == "   FEELWORDS   ")
@@ -40,25 +77,36 @@ namespace MyGame
             string line = "";
             for (int i = 0; i < text.Length; i++)
                 line += '═';
-            
+
+
+            CreateSpace(numSpace,  select,  counter);
             Fream('╔', line, '╗', numSpace);
+
+            CreateSpace(numSpace, select, counter);
             Fream('║', text, '║', numSpace);
+
+            CreateSpace(numSpace, select, counter);
             Fream('╚', line, '╝', numSpace);
         }
 
         static void Fream(char oneChar, string text, char twoChar,int numSpace)
         {
-            for (int i = 0; i < numSpace; i++)
-                Console.Write(" ");
+            Console.Write(" ");
 
-            //if(text == " Новая игра ")
-            //    Console.BackgroundColor = ConsoleColor.Red;
-            
             Console.Write(oneChar);
             Console.Write(text);
             Console.WriteLine(twoChar);
 
-            //Console.ResetColor();
+            Console.ResetColor();
+        }
+
+        static void CreateSpace(int numSpace, int select, int counter)
+        {
+            for (int i = 0; i < numSpace; i++)
+                Console.Write(" ");
+
+            if (select == counter)
+                Console.BackgroundColor = ConsoleColor.Red;
         }
     }
 }
